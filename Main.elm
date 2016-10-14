8c28d66eb9376e83aad36ff462f2c6dae12bc290
Max New <maxsnew@gmail.com>
2016-10-14 14:56:14 -0400

    shows my fav stations, but no error handling

--- Main.elm
+++ Main.elm
@@ -1,4 +1,6 @@
-import Html exposing (Html, button, div, text)
+import Dict
+import Dict exposing (Dict)
+import Html exposing (Html)
 import Html.App as App
 import Html.Events exposing (onClick)
 import Http
@@ -8,13 +10,25 @@ import Task
 
 -- | Displays the current bike capacity for the hubway stations I care about
 
+-- | Constants
+
+type alias ID   = String
+type alias Name = String
+-- https://gbfs.thehubway.com/gbfs/en/station_information.json
+-- could do this more automatically
+stations_i_care_about : Dict ID Name
+stations_i_care_about = Dict.fromList [ ("5", "Northeastern North Parking Lot")
+                                      , ("160", "Wentworth")
+                                      , ("66", "Griggs Street")
+                                      , ("103", "JFK at Harvard Street") ]
+
 main =
   App.program { init = init, view = view, update = update , subscriptions = \_ -> Sub.none }
 
 
 -- MODEL
 
-type alias StationStatus = { station_id : String
+type alias StationStatus name = { station_id : name
                            , num_bikes_available : Int
                            -- , num_bikes_disabled : Int
                            , num_docks_available : Int
@@ -25,42 +39,73 @@ type alias StationStatus = { station_id : String
                            -- , last_reported : String -- Time
                            -- , eightd_has_available_keys : Boolean
                            }
-type alias Model = Result Http.Error (List StationStatus)
+type alias State =
+    Result Http.Error
+           (List (StationStatus ID))
 
-mk_station_status : String -> Int -> Int -> StationStatus
+mk_station_status : name -> Int -> Int -> StationStatus name
 mk_station_status id bikes docks =
     { station_id = id
     , num_bikes_available = bikes
     , num_docks_available  = docks
     }
 
-station_status : Decoder StationStatus
+station_status : Decoder (StationStatus ID)
 station_status = Decode.object3 mk_station_status
                  ("station_id" := Decode.string)
                  ("num_bikes_available" := Decode.int)
                  ("num_docks_available" := Decode.int)
 
-model_decoder : Decoder (List StationStatus)
+model_decoder : Decoder (List (StationStatus ID))
 model_decoder = at ["data", "stations"] (Decode.list station_status)
 
 fetch : Cmd Msg
 fetch = Task.perform  Err Ok <|
         Http.get model_decoder "https://gbfs.thehubway.com/gbfs/en/station_status.json"
     
-init : (Model, Cmd Msg)
+init : (State, Cmd Msg)
 init = (Ok [], fetch)
 
 -- UPDATE
 
-type alias Msg = Model
+type alias Msg = State
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> State -> (State, Cmd Msg)
 update new_info old_info = (new_info, Cmd.none)
 
 -- VIEW
 
-view : Model -> Html Msg
+view_station : StationStatus Name -> List (Html a)
+view_station station =
+    List.map (Html.td [] << (\x -> [x]))
+    [ Html.text (station.station_id)
+    , Html.text (toString station.num_bikes_available)
+    , Html.text (toString station.num_docks_available)
+    ]
+
+header : Html a
+header = Html.thead [] [Html.tr [] [
+                              Html.th [] [(Html.text "Station")]
+                            , Html.th [] [(Html.text "Bikes")]
+                            , Html.th [] [(Html.text "Vacancies")]
+                            ]]
+
+filter_stations : List (StationStatus ID) -> List (StationStatus Name)
+filter_stations ss =
+    let do_i_care stat =
+            case Dict.get stat.station_id stations_i_care_about of
+                Nothing   -> Nothing
+                Just name -> Just { stat | station_id = name }
+    in List.filterMap do_i_care ss
+
+view : State -> Html Msg
 view model =
-  div []
-    [ text (toString model)
+  Html.div []
+    [
+     case model of
+         Err err -> Html.text (toString err)
+         Ok stations ->
+         let relevant_stations = filter_stations stations
+             rows = List.map (Html.tr [] << view_station) relevant_stations
+         in Html.table [] [header , Html.tbody [] rows]
     ]
